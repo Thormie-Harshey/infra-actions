67e91bca9b06d6aeef5c640255efcb6b38dc793f
Thormie Harshey <ashayeadetomiwa77@gmail.com>
2025-05-30 14:20:31 +0100

    iac-actions

---
+++ terraform/terraform.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = "~> 1.0"
+  backend "s3" {
+    bucket = "eks-github-action"
+    key    = "iac-actions/state.tfstate"
+    region = "us-east-1"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.7"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.3"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.16"
+    }
+  }
+}
